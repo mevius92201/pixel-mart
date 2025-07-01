d8f4e8cd704725f71682c771fbac5e38e13a46eb
Chun <mevius92201@gmail.com>
2025-07-02 00:54:57 +0800

    feat: add MouseParallaxWrapper component; integrate into MapParallax for enhanced visual effects; update styles for star image and YouTube wrapper; refine animations in news_parallax.css

--- src/Component/pages/home/map/MapParallax.jsx
+++ src/Component/pages/home/map/MapParallax.jsx
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import "../../../../assets/map.css";
 import VideoPlayer from "./VideoPlayer";
-
+import MouseParallaxWrapper from "../../../mouseParallax";
 function Map() {
   return (
     <section className="map">
@@ -11,6 +11,9 @@ function Map() {
           <VideoPlayer videoId="D0UZyphq1U8" />
           <div className="floating-island"></div>
         </div>
+        <MouseParallaxWrapper strength={10}>
+          <img src="/src/assets/images/starssss.png" className="starsss" />
+        </MouseParallaxWrapper>
       </div>
     </section>
   );
