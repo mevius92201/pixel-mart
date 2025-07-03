223cc30b5ab142ac65a92b6423fca47e6512a1c9
Chun <mevius92201@gmail.com>
2025-07-03 17:45:38 +0800

    feat: update MapParallax component and styles; adjust MouseParallaxWrapper strength, refine layout, and enhance visual elements in map.css

--- src/Component/pages/home/map/MapParallax.jsx
+++ src/Component/pages/home/map/MapParallax.jsx
@@ -1,4 +1,3 @@
-import { useEffect } from "react";
 import "../../../../assets/map.css";
 import VideoPlayer from "./VideoPlayer";
 import MouseParallaxWrapper from "../../../mouseParallax";
@@ -7,11 +6,12 @@ function Map() {
     <section className="map">
       <div className="map-wrapper">
         <div className="land-wrapper">
-          <div className="floating-book"></div>
-          <VideoPlayer videoId="D0UZyphq1U8" />
+          <div className="floating-book">
+            <VideoPlayer videoId="D0UZyphq1U8" />
+          </div>
           <div className="floating-island"></div>
         </div>
-        <MouseParallaxWrapper strength={10}>
+        <MouseParallaxWrapper strength={20}>
           <img src="/src/assets/images/starssss.png" className="starsss" />
         </MouseParallaxWrapper>
         <div className="cloud-wrapper-down"></div>
